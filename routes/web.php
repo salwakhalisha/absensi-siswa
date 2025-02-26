05188f5afa287944a6c72359511a489ec955b501
unknown <salwakhalisha2023@gmail.com>
2025-02-26 10:47:31 +0700

    membuat tampilan admin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function ()
+{
+    return view('index',
+    [
+        "menu"=>"index"
+    ]);
+
+})->name('index');
