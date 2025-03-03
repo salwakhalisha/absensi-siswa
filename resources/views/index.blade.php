64b0c17ab35e26b215435d504842a65b748113fc
unknown <salwakhalisha2023@gmail.com>
2025-03-03 22:21:53 +0700

    merubah tampilan admin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Corona Admin</title>
+    <title>Absensi Siswa</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{asset('dist/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
     <link rel="stylesheet" href="{{asset('dist/assets/vendors/ti-icons/css/themify-icons.css')}}">
@@ -156,7 +156,7 @@
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar p-0 fixed-top d-flex flex-row">
             <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo-mini" href="{{route('index')}}"><img src="{{asset('assets/images/logo-mini.svg')}}" alt="logo" /></a>
+            <a class="navbar-brand brand-logo-mini" href="{{route('index')}}"><img src="{{asset('dist/assets/images/logo-mini.svg')}}" alt="logo" /></a>
             </div>
             <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -226,7 +226,7 @@
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item preview-item">
                 <div class="preview-thumbnail">
-                  <img src="{{asset('assets/images/faces/face4.jpg')}}" alt="image" class="rounded-circle profile-pic">
+                  <img src="{{asset('dist/assets/images/faces/face4.jpg')}}" alt="image" class="rounded-circle profile-pic">
                 </div>
                 <div class="preview-item-content">
                   <p class="preview-subject ellipsis mb-1">Mark send you a message</p>
@@ -236,7 +236,7 @@
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item preview-item">
                 <div class="preview-thumbnail">
-                  <img src="{{asset('assets/images/faces/face2.jpg')}}" alt="image" class="rounded-circle profile-pic">
+                  <img src="{{asset('dist/assets/images/faces/face2.jpg')}}" alt="image" class="rounded-circle profile-pic">
                 </div>
                 <div class="preview-item-content">
                   <p class="preview-subject ellipsis mb-1">Cregh send you a message</p>
@@ -246,7 +246,7 @@
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item preview-item">
                 <div class="preview-thumbnail">
-                  <img src="{{asset('assets/images/faces/face3.jpg')}}" alt="image" class="rounded-circle profile-pic">
+                  <img src="{{asset('dist/assets/images/faces/face3.jpg')}}" alt="image" class="rounded-circle profile-pic">
                 </div>
                 <div class="preview-item-content">
                   <p class="preview-subject ellipsis mb-1">Profile picture updated</p>
@@ -307,7 +307,7 @@
               <li class="nav-item dropdown">
               <a class="nav-link" id="profileDropdown" href="#" data-bs-toggle="dropdown">
                 <div class="navbar-profile">
-                <img class="img-xs rounded-circle" src="{{asset('assets/images/faces/salwa.jpg')}}" alt="">
+                <img class="img-xs rounded-circle" src="{{asset('dist/assets/images/faces/salwa.jpg')}}" alt="">
                     <p class="mb-0 d-none d-sm-block navbar-profile-name">Salwa Khalisha</p>
                     <i class="mdi mdi-menu-down d-none d-sm-block"></i>
                   </div>
@@ -625,144 +625,6 @@
                 </div>
               </div>
             </div>
-            <div class="row ">
-              <div class="col-12 grid-margin">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Order Status</h4>
-                    <div class="table-responsive">
-                      <table class="table">
-                        <thead>
-                          <tr>
-                            <th>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input" id="check-all">
-                                </label>
-                              </div>
-                            </th>
-                            <th> Client Name </th>
-                            <th> Order No </th>
-                            <th> Product Cost </th>
-                            <th> Project </th>
-                            <th> Payment Mode </th>
-                            <th> Start Date </th>
-                            <th> Payment Status </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input">
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="{{asset('dist/assets/images/faces/face1.jpg')}}" alt="image" />
-                              <span class="ps-2">Salwa Khalisha</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Dashboard </td>
-                            <td> Credit card </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-success">Approved</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input">
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="{{asset('dist/assets/images/faces/face2.jpg')}}" alt="image" />
-                              <span class="ps-2">Estella Bryan</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Website </td>
-                            <td> Cash on delivered </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-warning">Pending</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input">
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="{{asset('dist/assets/images/faces/face5.jpg')}}" alt="image" />
-                              <span class="ps-2">Lucy Abbott</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> App design </td>
-                            <td> Credit card </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-danger">Rejected</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input">
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="{{asset('dist/assets/images/faces/face3.jpg')}}" alt="image" />
-                              <span class="ps-2">Peter Gill</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Development </td>
-                            <td> Online Payment </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-success">Approved</div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <div class="form-check form-check-muted m-0">
-                                <label class="form-check-label">
-                                  <input type="checkbox" class="form-check-input">
-                                </label>
-                              </div>
-                            </td>
-                            <td>
-                              <img src="{{asset('dist/assets/images/faces/face4.jpg')}}" alt="image" />
-                              <span class="ps-2">Sallie Reyes</span>
-                            </td>
-                            <td> 02312 </td>
-                            <td> $14,500 </td>
-                            <td> Website </td>
-                            <td> Credit card </td>
-                            <td> 04 Dec 2019 </td>
-                            <td>
-                              <div class="badge badge-outline-success">Approved</div>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
             <div class="row">
               <div class="col-md-6 col-xl-4 grid-margin stretch-card">
                 <div class="card">
@@ -779,7 +641,7 @@
                       <div class="preview-item-content d-flex flex-grow">
                         <div class="flex-grow">
                         <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Leonard</h6>
+                          <h6 class="preview-subject">T. Rizka Namira</h6>
                           <p class="text-muted text-small">5 minutes ago</p>
                         </div>
                         <p class="text-muted">Well, it seems to be working now.</p>
@@ -793,7 +655,7 @@
                       <div class="preview-item-content d-flex flex-grow">
                         <div class="flex-grow">
                         <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Luella Mills</h6>
+                          <h6 class="preview-subject">Nadya Kaputri</h6>
                           <p class="text-muted text-small">10 Minutes Ago</p>
                         </div>
                         <p class="text-muted">Well, it seems to be working now.</p>
@@ -807,7 +669,7 @@
                       <div class="preview-item-content d-flex flex-grow">
                         <div class="flex-grow">
                         <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Ethel Kelly</h6>
+                          <h6 class="preview-subject">Nadya Febriasari</h6>
                           <p class="text-muted text-small">2 Hours Ago</p>
                         </div>
                         <p class="text-muted">Please review the tickets</p>
@@ -821,7 +683,7 @@
                       <div class="preview-item-content d-flex flex-grow">
                         <div class="flex-grow">
                         <div class="d-flex d-md-block d-xl-flex justify-content-between">
-                          <h6 class="preview-subject">Herman May</h6>
+                          <h6 class="preview-subject">Sri Mutia Wahyuni</h6>
                           <p class="text-muted text-small">4 Hours Ago</p>
                         </div>
                         <p class="text-muted">Thanks a lot. It was easy to fix it .</p>
